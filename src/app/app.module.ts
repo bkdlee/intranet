908e956edd501730526e66ffb1903b93f8bf02ed
Jay Lee <bkd.lee@gmail.com>
2017-09-06 21:56:37 +1200

    Add Map and Home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NguiMapModule} from '@ngui/map';
 
 import { AppComponent } from './app.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
@@ -14,33 +15,40 @@ import { LoginBoxComponent } from './login-box/login-box.component';
 import { MenuTopComponent } from './menu/menu-top/menu-top.component';
 import { MenuSideComponent } from './menu/menu-side/menu-side.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { MapComponent } from './map/map.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-		{ path: '', redirectTo:'home', pathMatch:'full'},
-		{ path: 'calendar', component: CalendarComponent}
+	{ path: '', redirectTo:'home', pathMatch:'full'},
+	{ path: 'home', component: HomeComponent},
+	{ path: 'calendar', component: CalendarComponent},
+	{ path: 'map', component: MapComponent}
 ];
 
 @NgModule({
-		declarations: [
-				AppComponent,
-				BreadcrumbComponent,
-				ContentComponent,
-				FooterComponent,
-				LoginBoxComponent,
-				MenuTopComponent,
-				MenuSideComponent,
-				CalendarComponent
-		],
-		imports: [
-				BrowserModule,
-				FormsModule,
-				ReactiveFormsModule,
-				HttpModule,
-				BrowserAnimationsModule,
-				NgZorroAntdModule.forRoot(),
-				RouterModule.forRoot(routes)
-		],
-		providers: [],
-		bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		BreadcrumbComponent,
+		ContentComponent,
+		FooterComponent,
+		LoginBoxComponent,
+		MenuTopComponent,
+		MenuSideComponent,
+		CalendarComponent,
+		MapComponent,
+		HomeComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		HttpModule,
+		BrowserAnimationsModule,
+		NgZorroAntdModule.forRoot(),
+		RouterModule.forRoot(routes),
+		NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js'})
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
